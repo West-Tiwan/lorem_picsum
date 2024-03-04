04a39776ac2c04a77cb2c8892c782eeae202b5e5
West-Tiwan <xyzmohit.singh@gmail.com>
2024-03-05 00:52:18 +0530

    images alt

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ const Page = () => {
             <button onClick={getimg}>get images</button>
             <div>
                 {imag.map((elem,index)=>{
-                    return <img key={index} src={elem.download_url} />
+                    return <img key={index} src={elem.download_url} alt={}/>
                 })}
             </div>
         </>
