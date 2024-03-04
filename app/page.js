895c813d5a71054a8d351575f0f58c0500ecb221
West-Tiwan <xyzmohit.singh@gmail.com>
2024-03-05 00:51:41 +0530

    images comes on screen

--- app/page.js
+++ app/page.js
@@ -1,17 +1,22 @@
 "use client"
-import React from 'react'
+import React, {useState} from 'react'
 import axios from "axios";
 
 const Page = () => {
+    const [imag, setImag] = useState([]);
+
     const getimg = async  ()=> {
         const  response = await axios.get("https://picsum.photos/v2/list")
         console.log(response)
+        setImag(response.data)
     }
     return (
         <>
             <button onClick={getimg}>get images</button>
             <div>
-
+                {imag.map((elem,index)=>{
+                    return <img key={index} src={elem.download_url} />
+                })}
             </div>
         </>
     )
